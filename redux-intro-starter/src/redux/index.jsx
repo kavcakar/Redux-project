b095971665ca2605ac11905b7929d3a34a905d0d
Emre kavcakar <kavcakaremre@gmail.com>
2023-07-09 01:11:47 -0400

    created store

--- redux-intro-starter/src/redux/index.jsx
+++ redux-intro-starter/src/redux/index.jsx
@@ -3,16 +3,18 @@ const initialState = {
      counter: 0
 }
 
-export const reducer = {state = initialState, action} => {
+const reducer = {state = initialState, action} => {
     switch (action.type) {
         case "INCREASE":
             return {counter:state.counter + 1}
         case "DECREASE":
             return {counter:state.counter - 1}
             case "RESET":
-                return {counter:state.counter 0}
+                return {counter: 0}
     
         default:
             return state
     }
 }
+
+export default reducer
