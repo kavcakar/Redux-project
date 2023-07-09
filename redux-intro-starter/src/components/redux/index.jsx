82b90db377914a6ee7a462f2a94b9bf26482f930
Emre kavcakar <kavcakaremre@gmail.com>
2023-07-09 00:32:25 -0400

    initial state

---
+++ redux-intro-starter/src/components/redux/index.jsx
@@ -0,0 +1,15 @@
+
+const initialState = {
+     counter: 0
+}
+
+export const reducer = {state = initialState, action} => {
+    switch (key) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
