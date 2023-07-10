0e87f0cbfdb97f05e9c3a993dfa9f42d7808a761
Emre kavcakar <kavcakaremre@gmail.com>
2023-07-10 12:44:09 -0400

    todoReducer

--- redux-intro-starter/src/redux/index.jsx
+++ redux-intro-starter/src/redux/index.jsx
@@ -1,20 +1,21 @@
+//  const initialState = {
+//    counter: 0, //? Global state
+// }
 
-const initialState = {
-     counter: 0
-}
+// // //? Pure js function
+// // //? Bir dis bagimlilik olmamali
+//  const reducer = (state = initialState, action) => {
+//    switch (action.type) {
+//      case "INCREASE":
+//        return { counter: state.counter + 1 }
+//      case "DECREASE":
+//        return { counter: state.counter - 1 }
+//      case "RESET":
+//        return { counter: 0 }
+//      default:
+//        return state
+//    }
+//  }
+ 
 
-const reducer = {state = initialState, action} => {
-    switch (action.type) {
-        case "INCREASE":
-            return {counter:state.counter + 1}
-        case "DECREASE":
-            return {counter:state.counter - 1}
-            case "RESET":
-                return {counter: 0}
-    
-        default:
-            return state
-    }
-}
-
-export default reducer
+// export default reducer
