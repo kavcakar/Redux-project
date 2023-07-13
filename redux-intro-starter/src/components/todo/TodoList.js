c7fc5c3c8b1f0d3bcbe15cfb3f203e63afa62bdf
Emre kavcakar <kavcakaremre@gmail.com>
2023-07-13 18:06:45 -0400

    todoList

--- redux-intro-starter/src/components/todo/TodoList.js
+++ redux-intro-starter/src/components/todo/TodoList.js
@@ -1,12 +1,15 @@
+import { useSelector } from "react-redux";
 import TodoItem from "./TodoItem";
 
 const TodoList = () => {
+  const todoList = useSelector((state) => state.todo.todoList)
+  
   const handleClearList = () => {};
 
   return (
     <div>
       <div>
-        {[].map((todo) => (
+        {todoList.map((todo) => (
           <TodoItem key={todo.id} {...todo} />
         ))}
       </div>
